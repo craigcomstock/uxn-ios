6bf65030a05542e59f30e1198a9bf8e7fb6c05dc
Craig Comstock <craig@unreasonablefarm.org>
2022-12-25 21:36:35 -0600

    rebuild roms with latest uxnasm

---
+++ assemble-roms.sh
@@ -0,0 +1,13 @@
+set -x
+cd uxn/projects/examples
+pwd
+for f in $(find . -name '*.tal'); do
+  echo $f
+  d=$(dirname $f)
+  mkdir -p ../../../roms/$d
+  rom=$(basename $f .tal).rom
+  ../../bin/uxnasm $f ../../../roms/$d/$rom
+done
+#uxn/bin/uxnasm uxn/projects/examples/devices/screen.tal roms/devices/screen.rom
+cd -
+pwd
