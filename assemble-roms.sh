316c6ec2d8e8e47cfa45d13e4829195faa41867f
Craig Comstock <craig@unreasonablefarm.org>
2022-12-31 12:14:55 -0600

    some fixup of assemble script and a TON more roms from uxn project defaults

--- assemble-roms.sh
+++ assemble-roms.sh
@@ -1,14 +1,14 @@
 set -x
 set -e
-cd uxn/projects/examples
-pwd
+
+cd uxn
 for f in $(find . -name '*.tal'); do
   echo $f
-  d=$(dirname $f)
-  mkdir -p ../../../roms/$d
-  rom=$(basename $f .tal).rom
-  ../../bin/uxnasm $f ../../../roms/$d/$rom
+  if ! (echo $f | grep library) && ! (echo $f | grep assets); then
+    d=$(dirname $f)
+    mkdir -p ../roms/$d
+    rom=$(basename $f .tal).rom
+    bin/uxnasm $f ../roms/$d/$rom
+  fi
 done
-#uxn/bin/uxnasm uxn/projects/examples/devices/screen.tal roms/devices/screen.rom
 cd -
-pwd
